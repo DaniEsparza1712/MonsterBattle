46f301fd601d7c1e4e2b49ba8e46399fba7ea686
DaniEsparza1712 <0244218@up.edu.mx>
2024-04-18 10:30:42 -0600

    Connection
    Added connection on main menu

---
+++ Assets/Scripts/MainMenu/MenuFunctions.cs
@@ -0,0 +1,48 @@
+using System.Collections;
+using System.Collections.Generic;
+using TMPro;
+using UnityEngine;
+
+//Message 95 start battle
+public class MenuFunctions : MonoBehaviour
+{
+    private string _joinCode;
+    public string GetJoinCode => _joinCode;
+
+    public TMP_Text joinCodeTMP;
+    public TMP_InputField joinCodeInput;
+    public Server server;
+    public Client client;
+    
+    public void GenerateJoinCode()
+    {
+        _joinCode = Random.Range(1000, 1500).ToString();
+        joinCodeTMP.text = $"Join Code: {_joinCode}";
+    }
+
+    public void GetJoinCodeFromInput()
+    {
+        _joinCode = joinCodeInput.text;
+        Debug.Log(_joinCode);
+    }
+
+    public void SendJoinCode()
+    {
+        StartCoroutine(client.SendMessageToServer(GetJoinCode));
+    }
+
+    public void ListenForJoinCode()
+    {
+        StartCoroutine(server.ExpectMessageFromServer(int.Parse(GetJoinCode)));
+    }
+    
+    public void SendStartBattle()
+    {
+        StartCoroutine(server.SendThread(95));
+    }
+
+    public void ListenStartBattle()
+    {
+        StartCoroutine(client.ReceiveThread());
+    }
+}
